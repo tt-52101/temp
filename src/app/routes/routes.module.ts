94bf7302cb981621154e1d18741047785aecb704
ChqJourney <20310381@qq.com>
2019-04-21 19:16:28 +0800

    initial

---
+++ src/app/routes/routes.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '@shared';
+import { RouteRoutingModule } from './routes-routing.module';
+// dashboard pages
+import { DashboardComponent } from './dashboard/dashboard.component';
+// passport pages
+import { UserLoginComponent } from './passport/login/login.component';
+import { UserRegisterComponent } from './passport/register/register.component';
+import { UserRegisterResultComponent } from './passport/register-result/register-result.component';
+import { UserLockComponent } from './passport/lock/lock.component';
+// single pages
+import { CallbackComponent } from './callback/callback.component';
+
+const COMPONENTS = [
+  DashboardComponent,
+  // passport pages
+  UserLoginComponent,
+  UserRegisterComponent,
+  UserRegisterResultComponent,
+  UserLockComponent,
+  // single pages
+  CallbackComponent
+];
+const COMPONENTS_NOROUNT = [];
+
+@NgModule({
+  imports: [SharedModule, RouteRoutingModule],
+  declarations: [...COMPONENTS, ...COMPONENTS_NOROUNT],
+  entryComponents: COMPONENTS_NOROUNT
+})
+export class RoutesModule {}
