57c698bbac675e99886da121e0ca46a1f131e61d
ChqJourney <20310381@qq.com>
2019-07-05 00:16:03 +0800

    team page 80%

--- src/app/routes/home/team/team.component.ts
+++ src/app/routes/home/team/team.component.ts
@@ -1,15 +1,7 @@
-import { element } from 'protractor';
-import { StatusLabelComponent } from './../../../shared/components/status-label/status-label.component';
-import { ToolsModule } from './../../tools/tools.module';
-import { map } from 'rxjs/operators';
+
 import { zip } from 'rxjs';
-import {
-  ElementRef,
-  ChangeDetectorRef,
-  ChangeDetectionStrategy,
-  ViewChild,
-} from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Inject } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { format } from 'date-fns';
 
@@ -22,29 +14,41 @@ export class RoutesHomeTeamComponent implements OnInit {
     this.show = true;
   }
 
-  //  revenue Data from json
-  theMonth = new Date().getMonth();
-  theYear = new Date().getFullYear();
+  // four blocks
+  // block 1 年度完成revenue
+  percentYearRevenue = 0;
   budgetTotal = 0;
   actualTotoal = 0;
+  // block 2 年度Job In
+  jobInData: any[] = [];
+  jobInDataYear: any[] = [];
+  jobInYTD = 0;
+  // block 3 月度完成revenue
+  percentMonthRevenue = 0;
   budgetMonth = 0;
   actualMonth = 0;
-  monthData: any = [];
+  // block 4 月度Job In
+  jobInDataMonth: any[] = [];
+  jobInMTD = 0;
+  //  date var
+  theMonth = new Date().getMonth();
+  theYear = new Date().getFullYear();
+
+  revenueActual: any = [];
+  jobInActual:any=[];
   jobinData: any = [];
-  percentMonthRevenue = 0;
-  percentYearRevenue = 0;
+
   timeChartData: any = [];
 
-  latestUpdateDate: Date;
-  totalLiveQuantity = 0;
-  totalLiveAmount = 0;
+  // latestUpdateDate: Date;
+  // totalLiveQuantity = 0;
+  // totalLiveAmount = 0;
   RevenueTitle = '';
 
   ProjectNos = 'Project Nos';
   loading = false;
   engineersList = [];
   workload = 0;
-  projectNoPieData = [];
 
   totalTabs: any[] = [
     { key: 'Revenue', show: true },
@@ -59,34 +63,78 @@ export class RoutesHomeTeamComponent implements OnInit {
       this.show = true;
     }
   }
-  engineerTabs: any = [];
-  jobDataofThisYear:any[]=[];
-  trendsData:any[]=[];
-  trendsDataYear: any[] = [];
-  trendsDataMonth:any[]=[];
+
+  jobDataofThisYear: any[] = [];
+
   ngOnInit() {
     this.loading = true;
 
     zip(
       this.http.get(`biz/revenue/${this.theYear}`),
-      this.http.get('biz/EngineersStatus'),
+      this.http.get('biz/EngineersStatus/safety'),
       this.http.get('biz/jobtrends/team', {
-        from: '2019-01-01',
-        to: '2019-06-01',
+        from: new Date(this.theYear, this.theMonth, 1).toLocaleDateString(),
+        to: new Date().toLocaleDateString(),
       }),
     ).subscribe(
-      ([revenueYear, engineers, trends]) => {
-        console.log(trends);
+      ([rjJsonData, engineers, jonIn]) => {
+        // block 1 & 3
+        this.revenueActual = [];
+        this.RevenueTitle = 'Revenue (' + rjJsonData.unit + ')';
+        this.budgetMonth = rjJsonData.data[this.theMonth].Budget;
+        this.budgetTotal = [...rjJsonData.data].reduce(
+          (acc, cur) => acc + cur.Budget,
+          0,
+        );
+        this.actualTotoal = [...rjJsonData.data].reduce(
+          (acc, cur) => acc + cur.Actual,
+          0,
+        );
+        this.percentYearRevenue = Math.floor(
+          (this.actualTotoal * 100) / this.budgetTotal,
+        );
+
+        // block 2
+        const temp3: any[] = [];
+        [...rjJsonData.data].forEach(item => {
+          temp3.push({
+            x: item.Month,
+            y: item.JobInActual,
+          });
+          this.revenueActual.push({
+            x:item.Month,
+            y:item.Actual
+          });
+          this.jobInActual.push(
+            {
+              x:item.Month,
+              y:item.JobInActual
+            }
+          )
+        });
+        this.jobInDataYear = temp3;
+        const temp0 = this.jobInDataYear.reduce((acc, cur) => acc + cur.y, 0);
+        this.jobInYTD = temp0;
+        console.log(this.jobInDataYear);
+        console.log(this.jobInYTD);
+        // block 3
+        this.actualMonth = this.jobInDataMonth.reduce(
+          (acc, cur) => acc + cur.JobCompleteAmount,
+          0,
+        );
+        this.percentMonthRevenue = Math.floor(
+          (this.actualMonth * 100) / rjJsonData.data[this.theMonth].Budget,
+        );
+        // block 4
         const temp1: any[] = [];
         const temp2: any[] = [];
-        this.jobDataofThisYear=trends.Items;
-        trends.Items.forEach(element => {
+        jonIn.Items.forEach(element => {
           temp1.push({
             x: format(Date.parse(element.TheDay), 'YYYY-MM-DD'),
             y: element.JobOpenCount,
           });
         });
-        const beginDay = new Date(2019, 0, 1).getTime();
+        const beginDay = new Date(this.theYear, this.theMonth, 1).getTime();
         const endDay = new Date().getTime();
         const dayCount = Math.abs(endDay - beginDay) / 1000 / 60 / 60 / 24;
         for (let i = 0; i < dayCount; i += 1) {
@@ -104,48 +152,18 @@ export class RoutesHomeTeamComponent implements OnInit {
             });
           }
         }
-        this.trendsDataYear = temp2;
-        this.trendsDataMonth=this.getFinshedStatusofThisMonth(this.trendsDataYear);
+        this.jobInDataMonth = temp2;
+        const tempMTD = this.jobInDataMonth.reduce(
+          (acc, cur) => acc + cur.y,
+          0,
+        );
+        this.jobInMTD=tempMTD;
+        this.jobInYTD = this.jobInYTD + this.jobInMTD;
+        console.log(this.jobInMTD);
 
-        this.trendsData=this.trendsDataMonth;
-        const thisMonthCom=this.getFinshedStatusofThisMonth(this.jobDataofThisYear);
-        console.log(thisMonthCom);
-        this.actualMonth = thisMonthCom
-          .reduce((acc, cur) => acc + cur.JobCompleteAmount,
-          0,);
-        this.monthData = [];
-        this.RevenueTitle = 'Revenue (' + revenueYear.unit + ')';
-        const monthInt = new Date().getMonth();
-        this.budgetMonth = revenueYear.data[monthInt].Budget;
-        this.budgetTotal = [...revenueYear.data].reduce(
-          (acc, cur) => acc + cur.Budget,
-          0,
-        );
-        this.actualTotoal = [...revenueYear.data].reduce(
-          (acc, cur) => acc + cur.Actual,
-          0,
-        );
-        this.percentYearRevenue = Math.floor(
-          (this.actualTotoal * 100) / this.budgetTotal,
-        );
-        this.percentMonthRevenue = Math.floor(
-          (this.actualMonth * 100) / revenueYear.data[this.theMonth].Budget,
-        );
-        // for bar chart of revenue
-        [...revenueYear.data].forEach(element => {
-          if (this.monthData.length !== 12) {
-            this.monthData.push({
-              x: element.Month,
-              y: element.Actual,
-            });
-          }
-          // for bar chart of job in
-          if (this.jobinData.length !== 12) {
-            this.jobinData.push({
-              x: element.Month,
-              y: element.JobIn,
-            });
-          }
+
+        // for chart of revenue
+        [...rjJsonData.data].forEach(element => {
           // for timline chart
           if (this.timeChartData.length !== 24) {
             this.timeChartData.push({
@@ -160,54 +178,57 @@ export class RoutesHomeTeamComponent implements OnInit {
             });
           }
         });
-        console.log(engineers);
+        
         this.engineersList = engineers.Items;
         this.loading = false;
       },
       err => (this.loading = false),
       () => {
-        const chart = new G2.Chart({
-          container: 'lineChart',
-          forceFit: true,
-          height: 290,
-          animate: false,
-        });
-        chart.source(this.timeChartData, {
-          month: {
-            range: [0, 1],
-          },
-        });
-        chart.tooltip({
-          crosshairs: {
-            type: 'line',
-          },
-        });
-        chart.axis('value', {
-          label: {
-            formatter: function formatter(val) {
-              return val + '￥';
-            },
-          },
-        });
-        chart
-          .line()
-          .position('month*value')
-          .color('valueType');
-        chart
-          .point()
-          .position('month*value')
-          .color('valueType')
-          .size(4)
-          .shape('circle')
-          .style({
-            stroke: '#fff',
-            lineWidth: 1,
-          });
-        chart.render();
+        this.renderChart(this.timeChartData);
       },
     );
   }
 
+  renderChart(chartData:any[]){
+    const chart = new G2.Chart({
+      container: 'lineChart',
+      forceFit: true,
+      height: 290,
+      animate: false,
+    });
+    chart.source(chartData, {
+      month: {
+        range: [0, 1],
+      },
+    });
+    chart.tooltip({
+      crosshairs: {
+        type: 'line',
+      },
+    });
+    chart.axis('value', {
+      label: {
+        formatter: function formatter(val) {
+          return val + '￥';
+        },
+      },
+    });
+    chart
+      .line()
+      .position('month*value')
+      .color('valueType');
+    chart
+      .point()
+      .position('month*value')
+      .color('valueType')
+      .size(4)
+      .shape('circle')
+      .style({
+        stroke: '#fff',
+        lineWidth: 1,
+      });
+    chart.render();
+  }
   engIdx = 0;
   selectChange(idx: number) {
     // if (this.totalTabs[idx].show !== true) {
@@ -216,34 +237,20 @@ export class RoutesHomeTeamComponent implements OnInit {
     // }
     console.log('change');
   }
-  engChange(index: number) {}
-  trendType='Trend of This Year';
-  getFinshedStatusofThisMonth(data:any[]){
-    const firstDayofThisMonth=new Date(new Date().getFullYear(),new Date().getMonth(),1);
-  //   console.log(new Date().getTime());
-  //   console.log(firstDayofThisMonth.getTime());
-  //   console.log(new Date().getTime()-firstDayofThisMonth.getTime());
-   const d=new Date().getTime()-firstDayofThisMonth.getTime();
+  
+  getFinshedStatusofThisMonth(data: any[]) {
+    const firstDayofThisMonth = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      1,
+    );
+    //   console.log(new Date().getTime());
+    //   console.log(firstDayofThisMonth.getTime());
+    //   console.log(new Date().getTime()-firstDayofThisMonth.getTime());
+    const d = new Date().getTime() - firstDayofThisMonth.getTime();
     const dayCount = Math.abs(Math.floor(d / 1000 / 60 / 60 / 24));
     console.log(dayCount);
     return data.slice(-dayCount);
   }
-  JobinChartChange(){
-    
-    if(this.trendType==='Trend of This Month'){
-      
-      this.trendsData=[];
-      this.trendsDataMonth.forEach(item=>this.trendsData.push(item));
-      console.log(this.trendsData);
-      this.trendType='Trend of This Year';
-    }else{
-      this.trendsData=[];
-      this.trendsDataYear.forEach(item=>this.trendsData.push(item));
-      console.log(this.trendsData);
-      this.trendType='Trend of This Month';
-    }
-    
-    
-
-  }
+  
 }
